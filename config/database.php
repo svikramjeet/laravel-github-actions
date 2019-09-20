99514ba4b7d0a2ca2f3e67793f5472a20868d355
Vikramjeet SIngh <vikramjeet@ucreate.co.in>
2019-09-20 20:56:12 +0530

    Update database.php

--- config/database.php
+++ config/database.php
@@ -6,11 +6,6 @@ $username = $url["user"];
 $password = $url["pass"];
 $database = substr($url["path"], 1);
 
-$redis_url      = parse_url(env("REDIS_URL"));
-$redis_host     = $redis_url["host"];
-$redis_port     = $redis_url["port"];
-$redis_password = $redis_url["pass"]=="null"?null:$redis_url["pass"];
-
 return [
 
     'fetch' => PDO::FETCH_CLASS,
@@ -28,16 +23,6 @@ return [
         ],
     ],
     'migrations' => 'migrations',
-        'redis' => [
-            'client' => 'predis',
-            'default' => [
-                'host'     => env('REDIS_HOST', $redis_host),
-                'password' => env('REDIS_PASSWORD', $redis_password),
-                'port'     => env('REDIS_PORT', $redis_port),
-                'database' => 0,
-                ],
-
-        ],
 ];
 
 ?>
